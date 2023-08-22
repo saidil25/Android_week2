a894f69830e221d33b25eb3e43f7272768d9088b
saidilhalim <saidilhalim@mail.ugm.ac.id>
2023-08-22 10:00:42 +0700

    Tugas Android

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            test1.setOnClickListener{
+                Toast.makeText(this@MainActivity,"Hello" +" "+textname.text,Toast.LENGTH_LONG).show()
+            }
+            test2.setOnClickListener{
+                Toast.makeText(this@MainActivity,"Hallo teman2",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
